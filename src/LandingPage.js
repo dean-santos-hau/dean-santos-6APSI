b824dba5adad5abdae60efb621bc91c7c9b137f0
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-06-24 20:23:02 +0800

    Added navigation bar

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,6 +3,13 @@ import './LandingPage.css';
 function LandingPage() {
   return (
     <div className="landing-page-container">
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Login</li>
+            </ul>
+        </nav>
         <h1>Im Landing Page</h1>
         <div className='img-container'></div>
     </div>

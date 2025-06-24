db528b7985e97cb34386af68db6ed04768e5bc42
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-06-24 20:49:02 +0800

    add new <div> className='landing-page-wrapper' and moved <nav> outside landing-page-container

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,7 +2,7 @@ import './LandingPage.css';
 
 function LandingPage() {
   return (
-    <div className="landing-page-container">
+    <div className='landing-page-wrapper'>
         <nav>
             <ul>
                 <li>Home</li>
@@ -10,8 +10,10 @@ function LandingPage() {
                 <li>Login</li>
             </ul>
         </nav>
-        <h1>Im Landing Page</h1>
-        <div className='img-container'></div>
+        <div className="landing-page-container">
+            <h1>Im Landing Page</h1>
+            <div className='img-container'></div>
+        </div>
     </div>
   );
 }

3cca68cd0a6c73dd6bdc3a12a099e99923427c45
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-06-24 21:18:45 +0800

    linked login-page to landing-page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 function LoginPage() {
- 
+  const navigate = useNavigate();
+  const handleLogin = () => {
+  navigate("/");
+  }
   return (
     <div className='login-page-wrapper'>
       <div className='login-container'>
@@ -25,6 +29,7 @@ function LoginPage() {
           <button 
             type='submit' 
             id="login-button"
+            onClick={handleLogin}
           >
             Login
           </button>

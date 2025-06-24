e414aa357ed691ccda130bac50794720a620fc81
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-06-24 19:55:54 +0800

    Added imports for routing and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter , Route, Routes } from "react-router-dom";
 import LoginPage from './LoginPage'; 
+import LandingPage from './LandingPage';
 
 function App() {
   return (
